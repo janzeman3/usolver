7694b27166e6b18ff95a3fda31f46b75a9fa853d
Jan <janzeman3@seznam.cz>
2017-09-01 22:01:57 +0200

    Basic requirements
    Signed-off-by: Jan <janzeman3@seznam.cz>

---
+++ doc/doc_requirements.py
@@ -0,0 +1,16 @@
+## @page Requirements Requirements
+#  user requirements on system
+#
+#  @section MainAim Main aim
+#  Main aim of the sowtware is provide universal solver of many problems.
+#
+#  @section Requiremnts Requirements
+#
+#  @subsection R1 Easy extensions
+#  Addition of new problem must be easy and done by user. While the list of problems is maintened by programmer.
+#
+#  @subsection R2 Complex problems
+#  Each problem can be of bigger complexity.
+#
+#  @subsection R3 GNU/GPL
+#  SW will be distributed under GNU/GPL licence. Most of SW will be free, but some parts can be customized and payed.
